b1b2e089c79315e4a69859a103970bcd0268aea8
johann <weytensjohann771@gmail.com>
2023-04-03 15:28:36 +0200

    TP logger + hellow world + début TP bdd

--- springcefim/src/main/java/fr/weytensjohann/springcefim/HelloController.java
+++ springcefim/src/main/java/fr/weytensjohann/springcefim/HelloController.java
@@ -1,5 +1,7 @@
 package fr.weytensjohann.springcefim;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/hello")
 
 public class HelloController {
+
+    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
     @RequestMapping("/world")
     public String helloWorld() {
+        logger.info("Hello World");
         return "Hello World";
+
     }
 }
